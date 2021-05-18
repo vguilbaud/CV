663e2cfd82f41d1b4fc86cd1bfab44d26d880db2
vguilbaud <vguilbaud@users.noreply.github.com>
2021-05-18 17:26:14 +0200

    ajout smooth scrolling

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// document.querySelector(".menu").addEventListener("click", function (e) {
+//   e.preventDefault();
+
+//   // Matching strategy
+//   if (e.target.classList.contains("menu__content")) {
+//     const id = e.target.getAttribute("href");
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   }
+// });
+
+document.querySelectorAll(".menu__link").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
